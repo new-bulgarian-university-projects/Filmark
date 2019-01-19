9e02b6b280d4116d231c1a5be27add73b8566f90
nkolchakov <nikola.kolchakov@gmail.com>
2019-01-19 12:59:20 +0200

    change icons

---
+++ app/src/main/java/bg/nbu/android/filmark/BookmarkListActivity.java
@@ -0,0 +1,15 @@
+package bg.nbu.android.filmark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookmarkListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookmark_list);
+
+
+    }
+}
